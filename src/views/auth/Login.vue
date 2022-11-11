c6348586f7e5a1503e180cdb0cf549d984af0253
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-11 22:36:35 +0430

    Project Setuped and Login, Register page done

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-card class="mt-16 pa-10" elevation="3">
+          <v-card-title class="d-flex justify-center">
+            <h2 class="mb-4">Sign In</h2>
+          </v-card-title>
+          <v-card-subtitle>
+            <router-link to="/register">Sign Up</router-link> for free
+          </v-card-subtitle>
+          <v-card-text>
+            <v-text-field
+              label="Email"
+              placeholder="Enter your email here"
+              type="email"
+              append-icon="mdi-email"
+              rounded
+              outlined
+              dense
+            >
+            </v-text-field>
+            <v-text-field
+              label="Password"
+              placeholder="Enter your password here"
+              type="password"
+              append-icon="mdi-lock"
+              rounded
+              dense
+              outlined
+            >
+            </v-text-field>
+            <v-checkbox color="success" label="Remeber me"></v-checkbox>
+
+            <v-btn dense block color="primary">
+              <v-icon>mdi-login</v-icon> Sign In</v-btn
+            >
+          </v-card-text>
+          <v-card-actions class="d-flex justify-center">
+            <router-link to="/" class="text-text-decoration-none"
+              >Forget Password</router-link
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-card class="mt-4 pa-6" elevation="3">
+          <v-card-title class="d-flex justify-center">
+            <h2 class="mb-4">Sign Up</h2>
+          </v-card-title>
+          <v-card-subtitle
+            ><router-link to="/login">Sign In</router-link>
+            here</v-card-subtitle
+          >
+          <v-card-text>
+            <v-text-field
+              label="Name"
+              placeholder="Enter your name here"
+              append-icon="mdi-account"
+              rounded
+              outlined
+              dense
+            ></v-text-field>
+            <v-text-field
+              label="Email"
+              placeholder="Enter email here"
+              append-icon="mdi-email"
+              outlined
+              dense
+              rounded
+            >
+            </v-text-field>
+            <v-text-field
+              label="Password"
+              placeholder="Enter your password here"
+              append-icon="mdi-lock"
+              type="password"
+              outlined
+              rounded
+              dense
+            ></v-text-field>
+            <v-text-field
+              label="Confirm Password"
+              placeholder="Enter confirm password here"
+              append-icon="mdi-lock"
+              type="password"
+              outlined
+              rounded
+              dense
+            >
+            </v-text-field>
+            <v-checkbox color="success" label="Remember me"></v-checkbox>
+            <v-btn color="primary" block
+              ><v-icon>mdi-login</v-icon> Sign Up</v-btn
+            >
+          </v-card-text>
+          <v-card-actions></v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
