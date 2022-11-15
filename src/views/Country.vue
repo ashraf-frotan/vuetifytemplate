053c5f9beab885c7737d393145d3fc04c7b20e19
Ahmad Reza Azimi <ashraffrotan36@gmail.com>
2022-11-15 16:58:53 +0430

    Dialogs added

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -8,7 +8,7 @@
               Countries
             </v-col>
             <v-col sm="12" md="6" lg="6" class="d-flex justify-end">
-              <v-btn small color="primary" elevation="5"
+              <v-btn small color="primary" elevation="5" @click="dialog=true"
                 ><v-icon small>mdi-plus</v-icon></v-btn
               >
               <v-btn small color="success" class="ml-1" elevation="5"
@@ -17,7 +17,7 @@
               <v-btn small color="orange" class="ml-1 white--text" elevation="5"
                 ><v-icon small>mdi-filter</v-icon></v-btn
               >
-              <v-btn small color="error" class="ml-1"
+              <v-btn small color="error" class="ml-1" @click="delete_dialog=true"
                 ><v-icon small>mdi-delete</v-icon></v-btn
               >
             </v-col>
@@ -62,7 +62,89 @@
         </v-card-actions>
       </v-card>
     </v-col>
+    <v-dialog
+      v-model="dialog"
+      max-width="300"
+    >
+      <v-card>
+        <v-card-title class="text-h9">
+          Register Country
+        </v-card-title>
+
+        <v-card-text>
+            <v-row>
+              <v-col cols="12" class="pa-3">
+                <v-text-field label="Name" placeholder="Enter country code here" v-model="name" dense rounded outlined></v-text-field>
+              </v-col>
+            </v-row>
+
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          
+          <v-btn
+            color="green"
+            @click="dialog = false"
+            small
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            small
+            color="primary"
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="delete_dialog"
+      max-width="300"
+    >
+      <v-card>
+        <v-card-title class="text-h9 d-flex justify-center">
+          <v-icon size="85" color="orange">mdi-alert-circle-outline</v-icon>
+        </v-card-title>
+
+        <v-card-text class="d-flex text-center">
+            <v-row>
+              <v-col cols="12" class="pa-3 text-h9">
+                <h3 class="text-h9">
+                  Are you sure to delete?
+                </h3>
+              </v-col>
+            </v-row>
+
+        </v-card-text>
+
+        <v-card-actions class="d-flex justify-center">
+
+          
+          <v-btn
+            color="primary"
+            @click="delete_dialog = false"
+            small
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            small
+            color="error"
+            @click="delete_dialog = false"
+          >
+            Yes
+          </v-btn>
+
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-row>
+  
 </template>
 
 <script>
@@ -75,8 +157,16 @@ export default {
         { id: 13, name: "Dubai" },
       ],
       page: 2,
+      dialog:false,
+      name:'',
+      delete_dialog:false
     };
   },
+  watch:{
+    dialog(){
+      this.name=''
+    }
+  }
 };
 </script>
 
