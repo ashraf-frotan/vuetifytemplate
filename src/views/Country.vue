875544f4ff85275b3fac53e72055b2fdca1ba801
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-16 19:16:35 +0430

    state datatable added

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -8,165 +8,175 @@
               Countries
             </v-col>
             <v-col sm="12" md="6" lg="6" class="d-flex justify-end">
-              <v-btn small color="primary" elevation="5" @click="dialog=true"
-                ><v-icon small>mdi-plus</v-icon></v-btn
-              >
-              <v-btn small color="success" class="ml-1" elevation="5"
-                ><v-icon small>mdi-pencil</v-icon></v-btn
-              >
-              <v-btn small color="orange" class="ml-1 white--text" elevation="5"
-                ><v-icon small>mdi-filter</v-icon></v-btn
-              >
-              <v-btn small color="error" class="ml-1" @click="delete_dialog=true"
-                ><v-icon small>mdi-delete</v-icon></v-btn
-              >
+              <v-tooltip bottom color="primary">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    small
+                    color="primary"
+                    elevation="5"
+                    v-bind="attrs"
+                    v-on="on"
+                    @click="dialog = true"
+                    ><v-icon small>mdi-plus</v-icon></v-btn
+                  >
+                </template>
+                <span>Add New</span>
+              </v-tooltip>
+              <v-tooltip bottom color="green">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    small
+                    color="success"
+                    class="ml-1"
+                    elevation="5"
+                    v-bind="attrs"
+                    v-on="on"
+                    ><v-icon small>mdi-pencil</v-icon></v-btn
+                  >
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
+
+              <v-tooltip bottom color="orange">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    small
+                    color="orange"
+                    class="ml-1 white--text"
+                    elevation="5"
+                    v-on="on"
+                    v-attrs="attrs"
+                    ><v-icon small>mdi-filter</v-icon></v-btn
+                  >
+                </template>
+                <span>Filter</span>
+              </v-tooltip>
+              <v-tooltip bottom color="error">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    small
+                    color="error"
+                    class="ml-1"
+                    @click="delete_dialog = true"
+                    v-on="on"
+                    v-bind="attrs"
+                    ><v-icon small>mdi-delete</v-icon></v-btn
+                  >
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
             </v-col>
           </v-row>
         </v-card-title>
         <v-card-text>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th>
-                    <v-checkbox
-                      class="checkbox"
-                      hide-details
-                      dense
-                    ></v-checkbox>
-                  </th>
-                  <th>ID</th>
-                  <th>Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in items" :key="item.id">
-                  <td>
-                    <v-checkbox
-                      class="checkbox"
-                      hide-details
-                      dense
-                    ></v-checkbox>
-                  </td>
-                  <td>{{ item.id }}</td>
-                  <td>{{ item.name }}</td>
-                </tr>
-              </tbody>
+          <v-data-table
+            v-model="selected"
+            :headers="headers"
+            :items="countries"
+            :items-per-page="5"
+            item-key="name"
+            show-select
+            :single-select="singleSelect"
+            class="elevation-1"
+          >
+            <template v-slot:top>
+              <v-switch
+                v-model="singleSelect"
+                label="Single select"
+                class="pa-3"
+              ></v-switch>
             </template>
-          </v-simple-table>
+          </v-data-table>
         </v-card-text>
         <v-card-actions>
           <div>
-            <v-pagination v-model="page" :length="3"></v-pagination>
+            <v-pagination v-model="page" :length="2"></v-pagination>
           </div>
         </v-card-actions>
       </v-card>
     </v-col>
-    <v-dialog
-      v-model="dialog"
-      max-width="300"
-    >
+    <v-dialog v-model="dialog" max-width="300">
       <v-card>
-        <v-card-title class="text-h9">
-          Register Country
-        </v-card-title>
+        <v-card-title class="text-h9"> Register Country </v-card-title>
 
         <v-card-text>
-            <v-row>
-              <v-col cols="12" class="pa-3">
-                <v-text-field label="Name" placeholder="Enter country code here" v-model="name" dense rounded outlined></v-text-field>
-              </v-col>
-            </v-row>
-
+          <v-row>
+            <v-col cols="12" class="pa-3">
+              <v-text-field
+                label="Name"
+                placeholder="Enter country code here"
+                v-model="name"
+                dense
+                rounded
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
         </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          
-          <v-btn
-            color="green"
-            @click="dialog = false"
-            small
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            small
-            color="primary"
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
-
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="green" @click="dialog = false" small> Cancel </v-btn>
+          <v-btn small color="primary" @click="dialog = false"> Save </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <v-dialog
-      v-model="delete_dialog"
-      max-width="300"
-    >
+    <v-dialog v-model="delete_dialog" max-width="300">
       <v-card>
         <v-card-title class="text-h9 d-flex justify-center">
           <v-icon size="85" color="orange">mdi-alert-circle-outline</v-icon>
         </v-card-title>
 
         <v-card-text class="d-flex text-center">
-            <v-row>
-              <v-col cols="12" class="pa-3 text-h9">
-                <h3 class="text-h9">
-                  Are you sure to delete?
-                </h3>
-              </v-col>
-            </v-row>
-
+          <v-row>
+            <v-col cols="12" class="pa-3 text-h9">
+              <h3 class="text-h9">Are you sure to delete?</h3>
+            </v-col>
+          </v-row>
         </v-card-text>
 
         <v-card-actions class="d-flex justify-center">
-
-          
-          <v-btn
-            color="primary"
-            @click="delete_dialog = false"
-            small
-          >
+          <v-btn color="primary" @click="delete_dialog = false" small>
             Cancel
           </v-btn>
-          <v-btn
-            small
-            color="error"
-            @click="delete_dialog = false"
-          >
+          <v-btn small color="error" @click="delete_dialog = false">
             Yes
           </v-btn>
-
         </v-card-actions>
       </v-card>
     </v-dialog>
   </v-row>
-  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      items: [
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "Name", value: "name" },
+      ],
+      countries: [
         { id: 11, name: "Afghanistan" },
         { id: 12, name: "Japan" },
         { id: 13, name: "Dubai" },
+        { id: 14, name: "Oman" },
+        { id: 15, name: "Germany" },
+        { id: 16, name: "China" },
       ],
-      page: 2,
-      dialog:false,
-      name:'',
-      delete_dialog:false
+      page: 1,
+      dialog: false,
+      name: "",
+      delete_dialog: false,
+      selected: [],
+      singleSelect: false,
     };
   },
-  watch:{
-    dialog(){
-      this.name=''
-    }
-  }
+  watch: {
+    dialog() {
+      this.name = "";
+    },
+  },
+  methods: {},
 };
 </script>
 
@@ -174,5 +184,8 @@ export default {
 .checkbox {
   margin: 0 !important;
   padding: 0 !important;
+}
+.v-data-table-header {
+  background-color: lightblue;
 }
 </style>
--- src/views/State.vue
+++ src/views/State.vue
@@ -1,12 +1,138 @@
 <template>
   <v-row>
-    <v-col> States here </v-col>
+    <v-col>
+      <v-card>
+        <v-card-title>
+          <v-row>
+            <v-col md="9"> States </v-col>
+            <v-col md="3">
+              <v-tooltip bottom color="purple">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    v-on="on"
+                    v-bind="attrs"
+                    x-small
+                    color="primary"
+                    fab
+                    elevation="9"
+                  >
+                    <v-icon>mdi-plus</v-icon>
+                  </v-btn>
+                </template>
+                <span>Add New</span>
+              </v-tooltip>
+              <v-tooltip color="pink" bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="success"
+                    x-small
+                    fab
+                    v-on="on"
+                    v-bind="attrs"
+                    class="ml-1"
+                  >
+                    <v-icon dense>mdi-pencil</v-icon>
+                  </v-btn>
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
+              <v-tooltip color="indigo" bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="orange"
+                    x-small
+                    fab
+                    dark
+                    v-on="on"
+                    v-bind="attrs"
+                    class="ml-1 white--text"
+                  >
+                    <v-icon dense>mdi-filter</v-icon>
+                  </v-btn>
+                </template>
+                <span>Filter</span>
+              </v-tooltip>
+              <v-tooltip color="cyan" bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="error"
+                    x-small
+                    fab
+                    v-on="on"
+                    v-bind="attrs"
+                    class="ml-1 white--text"
+                  >
+                    <v-icon dense>mdi-delete</v-icon>
+                  </v-btn>
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-card-text>
+          <v-data-table
+            :headers="headers"
+            :items="states"
+            item-key="states.id"
+            v-model="selected"
+            items-per-page="5"
+            show-select
+            :single-select="singleSelect"
+          >
+            <template v-slot:top>
+              <v-switch
+                v-model="singleSelect"
+                label="Single select"
+                class="pa-3"
+              ></v-switch>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <!-- Modal -->
+    <v-dialog v-model="dialog" width="400">
+      <v-card>
+        <v-card-title> Add New State </v-card-title>
+        <v-card-text>
+          <v-text-field
+            label="Name"
+            placeholder="Enter state name here"
+            outlined
+            dense
+          >
+          </v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-row>
 </template>
 
 <script>
 export default {
   name: "State",
+  data() {
+    return {
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "State", value: "state" },
+        { text: "Country", value: "country" },
+      ],
+      states: [
+        { id: 1, state: "kabul", country: "Afghanistan" },
+        { id: 2, state: "tehran", country: "Iran" },
+        { id: 3, state: "melborn", country: "Australlia" },
+        { id: 4, state: "Tokyo", country: "America" },
+        { id: 5, state: "Zorikh", country: "Swiss" },
+        { id: 6, state: "ghazni", country: "Afghanistan" },
+      ],
+      countries: ["Iran", "Afghanistan"],
+      selected: [],
+      singleSelect: false,
+      dialog: true,
+    };
+  },
 };
 </script>
 
